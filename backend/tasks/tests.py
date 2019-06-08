b43275cd79e282fa0bdc2f39cf24abd5fa366591
Zach Tibbitts <zachtib@gmail.com>
2019-06-08 17:36:33 -0400

    some more tests

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import TaskList, Task, Sharing
 
 
-class TaskListModelTestCase(TestCase):
+class TaskModelTestCase(TestCase):
 
     def setUp(self):
         self.user_1 = User.objects.create_user('user_1')
@@ -20,4 +20,14 @@ class TaskListModelTestCase(TestCase):
 
     def test_user_has_access_to_own_lists(self):
         allowed_lists = TaskList.get_allowed_for_user(self.user_1).all()
+        self.assertIn(self.user_1_list, allowed_lists)
         self.assertQuerysetEqual(allowed_lists, [repr(self.user_1_list)])
+
+    def test_user_cannot_access_other_lists(self):
+        allowed_lists = TaskList.get_allowed_for_user(self.user_1).all()
+        self.assertNotIn(self.user_2_list, allowed_lists)
+
+    def test_user_can_access_list_once_shared(self):
+        Sharing.objects.create(tasklist=self.user_1_list, user=self.user_2)
+        allowed_lists = TaskList.get_allowed_for_user(self.user_2).all()
+        self.assertIn(self.user_1_list, allowed_lists)
