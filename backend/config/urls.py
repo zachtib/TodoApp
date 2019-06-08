1bee20084ffcec578534c828b2d51226c4d8853f
Zach Tibbitts <zachtib@gmail.com>
2019-06-08 14:02:22 -0400

    DRF Authentication classes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include(router.urls)),
 ]
 
